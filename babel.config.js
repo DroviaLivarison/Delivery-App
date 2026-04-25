a12e01fc239409de2417e149dabda21cc739e42b
wlyd25 <drovia.livarison@gmail.com>
2026-04-25 18:28:48 +0300

    اصلاحات

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,8 @@
-// babel.config.js
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
@@ -20,7 +18,8 @@ module.exports = function(api) {
             '@utils': './src/utils'
           }
         }
-      ]
+      ],
+      'react-native-reanimated/plugin'  
     ]
   };
 };
